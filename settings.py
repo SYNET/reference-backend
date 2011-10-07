e0678a8b115f722e1139ca03a280cf384f9a3e3c
SYNET <synet@synesis.ru>
2011-10-07 16:59:55 +0300

    minor change in paths

--- settings.py
+++ settings.py
@@ -60,12 +60,12 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/synet/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/synet/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
